a7a64353b0b3246adabe3f18f07242ce34264282
jawad7ali <jawad7ali@gmail.com>
2019-10-07 00:13:25 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Grid, Card, Image } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GridExampleEqualWidthRow = () => (
+   
+        <Grid divided='vertically'>
+    <Grid.Row columns={6}>
+      <Grid.Column  textAlign='center'>
+        <Card image="/logo.png" />
+        <Card link header='Publisher Name'  />
+        <Card link header='Balance' meta='$5,567' />
+      </Grid.Column>
+      <Grid.Column  textAlign='center'>
+        <Card link header='Work Together'  />
+        <Card link header='My Account' />
+        <Card link header='User ID:' meta='356674' />
+      </Grid.Column>
+    </Grid.Row>
 
-export default App;
+    <Grid.Row columns={3}>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
+
+export default GridExampleEqualWidthRow
