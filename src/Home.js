4a397914a6dc16fc2f92847dd7ef7a0c09a37dee
jawad7ali <jawad7ali@gmail.com>
2019-10-09 00:43:33 +0500

    carasoul

--- src/Home.js
+++ src/Home.js
@@ -2,35 +2,46 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
-import { Grid, Card, Image, Segment } from 'semantic-ui-react';
+import { Grid, Card, Image, Segment, Container } from 'semantic-ui-react';
 
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+ 
 
 export class Logo extends Component {
   render() {
     return (
-      <Grid columns='equal'>
-        <Grid.Row>
-      <Grid.Column textAlign='center'> <Image centered  size='small' src='/logo.png' /></Grid.Column>
-      <Grid.Column textAlign='center'><Card className='work-together' color='teal' link header='Work Together'  /></Grid.Column>
-      <Grid.Column textAlign='center'><Card link header='Advertiser' meta='Promoted: 78' /></Grid.Column>
-      <Grid.Column textAlign='center'><Card link header='Money Earned:' meta='$23,450' /></Grid.Column>
-      <Grid.Column textAlign='center'><Card link header='Social Accounts'  /></Grid.Column>
-      <Grid.Column textAlign='center'><Card link header='Withdraw Funds'  /></Grid.Column>
+      <Grid columns={6} >
+    <Grid.Row stretched>
+      <Grid.Column textAlign='center'>
+      <Image centered  size='small' src='/logo.png' />
+      <Card link header='Publisher Name'  />
+      <Card link header='Balance' meta='$5,567' />
+      </Grid.Column>
+      <Grid.Column textAlign='center'>
+      <Card className='work-together' color='teal' link header='Work Together'  />
+      <Card link header='My Account'  />
+      <Card link header='User ID:' meta='356674'  />
+      </Grid.Column>
+      <Grid.Column textAlign='center' >
+         <Card centered link header='Advertiser' meta='Promoted: 78' /> 
+         <Card link header='No. Total Clicks:' meta='900,055' />
+      </Grid.Column>
+      <Grid.Column textAlign='center'>
+        <Card link header='Money Earned:' meta='$23,450' />
+        <Card link header='No. Total Impressions:' meta='3,657,566' />
+      </Grid.Column>
+      <Grid.Column textAlign='center'>
+        <Card link header='Social Accounts'  />
+        <Card link header='Find New Offers'  />
+      </Grid.Column>
+      <Grid.Column textAlign='center'>
+        <Card link header='Withdraw Funds'  />
+        <Card link header='Support'  />
+      </Grid.Column>
     </Grid.Row>
-    <Grid.Row>
-      <Grid.Column textAlign='center'><Card link header='Publisher Name'  /></Grid.Column>
-      <Grid.Column textAlign='center'><Card link header='My Account'  /></Grid.Column>
-      <Grid.Column textAlign='center'><Card link header='Advertiser' meta='Promoted: 78' /></Grid.Column>
-      <Grid.Column textAlign='center'><Card link header='Money Earned:' meta='$23,450' /></Grid.Column>
-      <Grid.Column textAlign='center'><Card link header='Social Accounts'  /></Grid.Column>
-      <Grid.Column textAlign='center'><Card link header='Withdraw Funds'  /></Grid.Column>
-    </Grid.Row>
-    <Grid.Row>
-      <Grid.Column textAlign='center'><Card link header='Balance' meta='$5,567' /></Grid.Column>
-      <Grid.Column><Card link header='User ID:' meta='356674'  /></Grid.Column>
-      <Grid.Column>Computer B Second</Grid.Column>
-      <Grid.Column textAlign='center'><Card link header='Publisher Name'  /></Grid.Column>
-    </Grid.Row> 
+   
+  
       </Grid>
     )
   }
@@ -39,11 +50,7 @@ export class Logo extends Component {
 export class WorkTogether extends Component {
   render() {   
     return (
-  <Grid>
-    <Grid.Row stretched>
-    
-    </Grid.Row>
-  </Grid>
+      <div></div>
     )
   }
 }
